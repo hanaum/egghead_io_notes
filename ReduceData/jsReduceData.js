a3354a69ae3e85628c90ccc6a5f361c8ac5e3282
hanaum <hanau12@gmail.com>
2016-08-03 18:16:51 -0700

    Add notes for creating an object with reduce.

--- ReduceData/jsReduceData.js
+++ ReduceData/jsReduceData.js
@@ -1,3 +1,4 @@
+// Tutorial example 1
 var data = [15,3,20];
 
 var reducer = function(accumulator, item) {
@@ -7,3 +8,29 @@ var initialValue = 0;
 
 var total = data.reduce(reducer, initialValue); // takes the reduced function and an initial value.
 // total = 38
+
+// Tutorial example 2
+var votes = [
+  'angular',
+  'angular',
+  'react',
+  'react',
+  'angular',
+  'ember',
+  'react',
+  'vanilla'
+];
+
+var initialValue = {};
+var reducer = function(tally, vote) {
+  if (!tally[vote]) {
+    tally[vote] = 1;
+  } else {
+    tally[vote] = tally[vote] + 1;
+  }
+  return tally;
+}
+
+var result = votes.reduce(reducer, initialValue);
+console.log(result);
+
