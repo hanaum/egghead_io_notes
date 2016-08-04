5024eccbc23fe882ae5c7f224f94c06535601a62
hanaum <hanau12@gmail.com>
2016-08-03 18:40:45 -0700

    Add notes for comparing the speed of reduce with map and filter.

--- ReduceData/jsReduceData.js
+++ ReduceData/jsReduceData.js
@@ -34,3 +34,27 @@ var reducer = function(tally, vote) {
 var result = votes.reduce(reducer, initialValue);
 console.log(result);
 
+// Tutorial example 3
+var data = [1,2,3];
+var doubled = data.reduce(function(acc, value) {
+  acc.push(value * 2);
+}, []);
+
+var doubleMapped = data.map(function(item) {
+  return item * 2;
+})
+// both of the above functions do the same thing
+console.log(doubled); // [2,4,6]
+
+// reduce is faster
+var data2 = [1,2,3,4,5,6,7,8];
+var evens = data2.reduce(function(acc, value) {
+  if (value % 2 === 0) {
+    acc.push(value * 2);
+  }
+  return acc;
+}, []);
+
+
+
+
